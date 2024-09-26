7e7a30b489a07ef8a5d9c6706834794da2d7581a
Pis4ek <thisisnikitas@gmail.com>
2024-09-26 16:38:03 +0300

    Created enviroment and start work with rope

---
+++ Assets/Project/Scripts/Gameplay/Player/RopeElement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Gameplay
+{
+    public class RopeElement : MonoBehaviour
+    {
+        [field: SerializeField] public SpriteRenderer Renderer { get; private set; }
+        [field: SerializeField] public CapsuleCollider2D Collider { get; private set; }
+        [field: SerializeField] public Rigidbody2D Rigidbody { get; private set; }
+        [field: SerializeField] public HingeJoint2D Joint { get; private set; }
+        [field: SerializeField] public Transform StartPoint { get; private set; }
+        [field: SerializeField] public Transform EndPoint { get; private set; }
+    }
+
+    public class Rope : MonoBehaviour
+    {
+        [SerializeField] RopeElement _ropePrefab;
+
+        private void Awake()
+        {
+            
+        }
+    }
+}
